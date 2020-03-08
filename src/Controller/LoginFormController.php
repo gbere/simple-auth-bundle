99df8371bff45a92d32177df815825bad356a275
Gerard Berengué <gerard@bere.cat>
2020-03-08 13:27:37 +0100

    add login form authenticator

---
+++ src/Controller/LoginFormController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gbere\Security\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+final class LoginFormController extends AbstractController
+{
+    /**
+     * @Route("/login", name="gbere_security_login")
+     */
+    public function __invoke(AuthenticationUtils $authenticationUtils)
+    {
+        $lastAuthError = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('frontend/login.html.twig', [
+            'last_username' => $lastUsername,
+            'last_auth_error' => $lastAuthError,
+        ]);
+    }
+}
