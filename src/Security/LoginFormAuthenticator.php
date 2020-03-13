0ec7426aef966c91634c2a007ec1ac9d66694263
Gerard Berengué <gerard@bere.cat>
2020-03-13 21:14:42 +0100

    onAuthenticationSuccess fallback to the / url

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -67,7 +67,7 @@ final class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implem
     public function getUser($credentials, UserProviderInterface $userProvider): User
     {
         $token = new CsrfToken('authenticate', $credentials['csrf_token']);
-        if (!$this->csrfTokenManager->isTokenValid($token)) {
+        if (false === $this->csrfTokenManager->isTokenValid($token)) {
             throw new InvalidCsrfTokenException();
         }
 
@@ -106,8 +106,7 @@ final class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implem
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse('/');
     }
 
     protected function getLoginUrl(): string
