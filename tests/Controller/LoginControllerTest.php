d8b040c44bdb1e6a8e2e5c5532abb65c2e6cd9e7
Gerard Berengué <gerard@bere.cat>
2020-03-16 22:55:34 +0100

    add test for login form controller

---
+++ tests/Controller/LoginControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gbere\Security\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class LoginControllerTest extends WebTestCase
+{
+    public function testLoginForm(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/login');
+        $client->submitForm('login', [
+            'email' => 'role-user@fixture.com',
+            'password' => 'role-user',
+        ]);
+        $this->assertResponseRedirects('/');
+    }
+}
