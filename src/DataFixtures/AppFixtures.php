fd01be25e29438e7cb5609183e040729086625e4
Gerard Berengué <gerard@bere.cat>
2020-04-07 13:35:40 +0200

    composer update

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gbere\SimpleAuth\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        // $manager->flush();
+    }
+}
