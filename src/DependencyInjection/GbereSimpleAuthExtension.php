0fc0f1cb4a348c9c5ba9837d0147d9714c1c4fea
Gerard Berengué <gerard@bere.cat>
2020-04-21 19:48:17 +0200

    fake routes of test on bundle extension

--- src/DependencyInjection/GbereSimpleAuthExtension.php
+++ src/DependencyInjection/GbereSimpleAuthExtension.php
@@ -7,10 +7,11 @@ namespace Gbere\SimpleAuth\DependencyInjection;
 use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class GbereSimpleAuthExtension extends Extension
+class GbereSimpleAuthExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * @throws Exception
@@ -22,5 +23,24 @@ class GbereSimpleAuthExtension extends Extension
 
         $configuration = $this->getConfiguration($configs, $container);
         $config = $this->processConfiguration($configuration, $configs);
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        if ('test' === $container->getParameter('kernel.environment')) {
+            // https://github.com/symfony/symfony/issues/24461#issuecomment-355839669
+            $extensionConfigsRefl = new \ReflectionProperty(ContainerBuilder::class, 'extensionConfigs');
+            $extensionConfigsRefl->setAccessible(true);
+            $extensionConfigs = $extensionConfigsRefl->getValue($container);
+            $extensionConfigs['security'][0]['access_control'] = array_merge(
+                [
+                    ['path' => '^/gbere-auth-test-role-admin', 'role' => 'ROLE_ADMIN'],
+                    ['path' => '^/gbere-auth-test-role-user', 'role' => 'ROLE_USER'],
+                ],
+                $extensionConfigs['security'][0]['access_control']
+            );
+
+            $extensionConfigsRefl->setValue($container, $extensionConfigs);
+        }
     }
 }
