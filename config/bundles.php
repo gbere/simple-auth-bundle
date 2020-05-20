089cfb11ab205236395ec7d8a201a7d52b426348
Gerard Berengué <gerard@bere.cat>
2020-05-20 18:13:57 +0200

    install php-translation only in dev

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Gbere\SimpleAuth\GbereSimpleAuthBundle::class => ['all' => true],
+    Translation\Bundle\TranslationBundle::class => ['dev' => true],
 ];
